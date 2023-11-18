decc7c24d80e2506c26b020544fce29e910db601
viruthika-varshini <139947381+viruthika-varshini@users.noreply.github.com>
2023-11-18 20:32:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from transformers import pipeline
+import gradio as gr
+
+model_checkpoint = "MuntasirHossain/RoBERTa-base-finetuned-emotion"
+
+model = pipeline("text-classification", model=model_checkpoint)
+
+
+def classify(text):
+    label = model(text)[0]["label"]
+    return label
+
+description = "This AI model is trained to classify texts expressing human emotion into six categories: sadness, joy, love, anger, fear, and surprise." 
+title = "Classify Texts Expressing Emotion"
+# theme = "peach"
+examples=[["This is such a beautiful place"]]
+
+gr.Interface(fn=classify,
+    inputs="textbox",
+    outputs="text",
+    title=title,
+    # theme = theme,
+    description=description,
+    examples=examples,
+).launch()
